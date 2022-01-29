0da73005cc75444735ee24d535b1e4427150bb43
LauraJeanneton <laura.jeanneton@etu.univ-amu.fr>
2022-01-29 16:00:50 +0100

    Creation de type TypeScript terminé

---
+++ gestionfacture/src/app/todo-list.component.ts
@@ -0,0 +1,30 @@
+// src/app/todo-list.component.ts
+
+import { Component, Input } from "@angular/core";
+import { Tasks } from './types/task';
+@Component({
+    // Ce composant sera affiché par Angular à chaque fois
+    // qu'un élément <app-todo-list> sera rencontré dans
+    // un template HTML
+    selector: 'app-todo-list',
+    // Le HTML reprend ici notre liste de tâches
+    template: `
+    <ul>
+    <li *ngFor="let item of tasks">
+        <label>
+        <input 
+            type="checkbox" 
+            id="item-{{ item.id }}" 
+            [checked]="item.done" 
+        />
+        {{ item.text }}
+        </label>
+    </li>
+</ul>
+    `
+})
+
+export class TodoListComponent {
+    @Input()
+    tasks: Tasks[] = []; 
+}
